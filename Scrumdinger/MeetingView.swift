ca63e6fa08cc6a8bd54925eae1ca2e96c958b8d8
Quentin Pubert <quentin.pubert@outlook.fr>
2021-10-02 13:14:56 +0200

    reached the stacks section of apple app dev training done with step 5

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Quentin PUBERT on 02/10/2021.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24, antialiased: true)
+            HStack {
+                Text("Speaker 1 of 3")
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+            .preferredColorScheme(.dark)
+    }
+}
