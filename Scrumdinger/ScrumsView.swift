aacfef728925cdd10f2c76449f4bf00aba3df342
Quentin Pubert <quentin.pubert@outlook.fr>
2021-10-03 16:15:52 +0200

    added ScrumsView with a list of CardViews

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Quentin PUBERT on 03/10/2021.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums, id: \.title) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.color)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
